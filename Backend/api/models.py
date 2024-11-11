06adfcb474f5a5394a6f1d55388e0ec1d6f491e4
SanAJC <146601230+SanAJC@users.noreply.github.com>
2024-11-11 12:01:06 -0500

    Backend final

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -35,7 +35,7 @@ class MyAccountManager(BaseUserManager):
         
 class User(AbstractBaseUser):
     username=models.CharField(max_length=50,unique=True)
-    email=models.CharField(max_length=100,unique=True)
+    email=models.CharField(max_length=50,unique=True)
     password=models.CharField(max_length=100)
     rol=models.CharField(max_length=50)
     
--- Backend/data_visualization/settings.py
+++ Backend/data_visualization/settings.py
@@ -101,8 +101,15 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'NAME': 'herramienta',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
